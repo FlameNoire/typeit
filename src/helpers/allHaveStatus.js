e4e8908ad4cf2f6cb227cb05d747186a97442e52
Alex MacArthur <alex.macarthur@daveramsey.com>
2018-12-06 12:13:59 -0600

    delete.

--- src/helpers/allHaveStatus.js
+++
@@ -1,9 +0,0 @@
-export default function (things, property, value) {
-  if (!things.length) return false;
-
-  let result = things.filter(thing => {
-    return thing.status[property] === value;
-  });
-
-  return result.length === things.length;
-}
--- src/helpers/toArrayOfNodes.js
+++
@@ -1,9 +0,0 @@
-export default function (thing) {
-  if (typeof thing === "string") {
-    thing = document.querySelectorAll(thing);
-  } else if (!(thing instanceof NodeList)) {
-    thing = [thing];
-  }
-
-  return [].slice.call(thing);
-}
